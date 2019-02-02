8d82354fee9956c7541a29c1d2b79630823ebf03
Harry Kwon <harrykwon20@gmail.com>
2019-02-02 02:33:07 -0500

    created Note model

--- controllers/notes-controller.js
+++ controllers/notes-controller.js
@@ -1,11 +1,20 @@
 var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.rulencoded({extended: true});
+var urlencodedParser = bodyParser.urlencoded({extended: true});
 
 //params: app - express app
 module.exports = function (app) {
-  app.get('/', function(req, res){
-    res.render('home');
+  //render view from data from database
+  app.get('/notes', function(req, res){
+    res.sendFile('/assets/index.html', {root: __dirname+'/..'});
+    //res.render('');
   });
+  
+  //create new note
+  app.post('/notes', function(req, res){
+    res.sendFile('/assets/index.html', {root: __dirname+'/..'});
+    //res.render('');
+  });
+  
           
 }
--- models/note.js
+++ models/note.js
@@ -1,8 +1,15 @@
+const uuidv4 = require('uuid/v4');
+
 var Note = function(data){
   this.data = data;
+  this.data.id = this.generateNodeId();
+  if(!this.data.name){
+  	this.data.name = 'default-name';
+  }
 };
 
-Note.prototype.data = {};
-
+Note.prototype.generateNodeId = function(data){
+  return uuidv4();
+};
 
 module.exports = Note;
