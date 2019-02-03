ac56181801ae0867b4134af067141178d7a74a06
Harry Kwon <harrykwon20@gmail.com>
2019-02-03 04:21:27 -0500

    small fix to delete ajax request

--- assets/notes.js
+++ assets/notes.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
 	$('.remove').on('click', function(){
     var obj = $(this);
+    var id = obj.attr('id');
     console.log('clicked remove button');
-    console.log('id: '+obj.attr('id'));
-    
-    var noteData = {id: obj.attr('id')};
+    console.log('id: '+id);
     
     $.ajax({
-      type: 'Post',
-      url: '/todo',
-      data: noteData,
+      type: 'DELETE',
+      url: '/todo/'+id,
       success: function(data){
         obj.parent().remove();
       }
