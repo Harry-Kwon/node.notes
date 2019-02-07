95dd4f37ee8814251a4f018dc5b3a8dd9d9b217a
Harry Kwon <harrykwon20@gmail.com>
2019-02-07 06:48:52 -0500

    added user signup frontend

---
+++ assets/users/signup.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  //check is username is valid when value of username input changes
+  $('form').on('submit', function(){
+    try{
+    let formData={};
+    formData.username=$(this).find('.username-input').val();
+   	formData.password=$(this).find('.password-input').val();
+    console.log(formData);
+    $.ajax({
+      type: 'POST',
+      url: '/users/action/signup',
+      data: formData,
+      success: function(data){
+        console.log('signup successful');
+      }
+    });
+    } catch(err){
+      console.log(err);
+      return false;
+    }
+    return false;
+  });
+  
+});
